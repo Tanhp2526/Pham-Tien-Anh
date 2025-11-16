8d5bff19d9af6f875921bd3d5881bd8e10fb9463
Tanhp2526 <anh2004zai@gmail.com>
2025-11-16 18:13:04 +0700

    commit 4

---
+++ hackerrank_data struct/reverse.py
@@ -0,0 +1,43 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_node(self,data):
+        node = Node(data)
+        if not self.head:
+            self.head = node
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = node
+    
+
+    def reverse_print(self):
+        current = self.head
+        stack = []
+
+        while current:
+            stack.append(current.data)
+            current = current.next
+
+        while stack:
+            print(stack.pop())
+
+n = int(input())
+
+for t in range(n):
+    cnt = int(input())
+    llist = LinkedList()
+    
+    for i in range(cnt):
+        node = int(input())
+        llist.insert_node(node)
+
+    
+    llist.reverse_print()
